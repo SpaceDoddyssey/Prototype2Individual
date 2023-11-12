1210bad49867064bb3f2d10d811a28c7fb61e949
Cameron Dodd <cadodd@ucsc.edu>
2023-11-11 20:28:12 -0800

    Walls spawning, player growing and shrinking

--- docs/CamDoddIndividualPrototype/main.js
+++ docs/CamDoddIndividualPrototype/main.js
@@ -15,28 +15,83 @@ options = {
   seed: 50,
 };
 
-/** @type {{pos: Vector, height: number, type: string}[]} */
+/** @type {{pos: Vector, width: number, height: number, type: string}[]} */
 let spawnedWalls;
 
 let nextWallDist = 0;
+let platformHeight = 10;
+
+// Player properties
+/** @type {{ pos: Vector, height: number, direction: number }} */
+let player; //Direction is 1 for growing, -1 for shrinking
+let playerWidth = 5;
 
 function update() {
-  //Initializer function
+  // Initializer function
   if (!ticks) {
-  
+    player = {
+      pos: vec(30, VIEW_Y - platformHeight), // Adjusted the starting position of the player
+      height: 10, // Initial height
+      direction: 1, // 1 for growing, -1 for shrinking
+    };
+
+    spawnedWalls = [];
   }
 
-  if(input.isJustPressed){
+  // Handle input
+  if (input.isJustPressed) {
     changeGrowthDirection();
   }
+
+  // Update player height
+  player.height += player.direction;
+  if (player.height <= 0) {
+    endGame();
+  }
+
+  // Spawn walls
+  nextWallDist--;
+  if (nextWallDist < 0) {
+    spawnWall();
+    nextWallDist = rnd(30, 60);
+  }
+
+  // Update and draw player
+  player.pos.y = VIEW_Y - 10 - player.height;
+  color("red");
+  rect(player.pos.x, player.pos.y, playerWidth, player.height);
+
+  // Update and draw walls
+  spawnedWalls.forEach((wall) => {
+    wall.pos.x -= 2;
+    wall.pos.y = VIEW_Y - platformHeight - wall.height; 
+    color("black");
+    line(wall.pos.x, wall.pos.y, wall.pos.x, VIEW_Y - platformHeight - 1);
+  });
+
+  // Draw the platform
+  color("blue");
+  rect(0, VIEW_Y - 10, VIEW_X, 10);
+
+  // Remove off-screen walls
+  spawnedWalls = spawnedWalls.filter((wall) => wall.pos.x + wall.width > 0);
 }
 
 //-----------------------------------
 //----------FUNCTIONS----------------
 //-----------------------------------
 
-function changeGrowthDirection(){
+function changeGrowthDirection() {
+  player.direction *= -1; // Toggle between growing and shrinking
+}
 
+function spawnWall() {
+  spawnedWalls.push({
+    pos: vec(VIEW_X, 180),
+    width: 5,
+    height: 10,
+    type: "ground"
+  });
 }
 
 function endGame() {
